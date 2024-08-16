0d423d6a8cb76879ef5eeabfd857ae6cb30b00c9
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2024-08-16 11:44:04 +0530

    renaming
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-printf "package transform\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/sample_fuzz/reverse_fuzz_test.go
+printf "package transform\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/sample-fuzz/reverse_fuzz_test.go
 go mod tidy
 compile_native_go_fuzzer sample_fuzz FuzzIsValidEmail FuzzIsValidEmail
