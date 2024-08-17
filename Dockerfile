a580d921e581b2584b31240b9a89efa459f0b0d2
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2024-08-17 17:46:36 +0530

    final
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # Use the OSS-Fuzz base image for Go projects
 FROM gcr.io/oss-fuzz-base/base-builder-go
 ENV FUZZING_LANGUAGE=go
-RUN git clone --depth 1 https://github.com/prady0t/sample-fuzz
+RUN git clone https://github.com/prady0t/sample-fuzz
 WORKDIR sample-fuzz
 COPY build.sh $SRC/
--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 printf "package transform\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/sample-fuzz/register.go
-ls -l
 go mod tidy
-go get github.com/prady0t/sample-fuzz/fuzz
 compile_native_go_fuzzer github.com/prady0t/sample-fuzz/fuzz FuzzIsValidEmail FuzzIsValidEmail
+ls
